48a295eb3d19472aa2582b9bfa7def1b6f495ea6
BhanuDeekshita-Anantha <bhanudeekshita599@gmail.com>
2022-01-11 11:04:59 +0530

    first commit

---
+++ external.js
@@ -0,0 +1,7 @@
+console.log(square(5));
+                function square(m){
+                  return siri(m,m);
+                }
+                function siri(a,b){
+                	return(a*b) 
+                }
